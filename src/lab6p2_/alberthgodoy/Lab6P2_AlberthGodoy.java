ded7acf49deec76382f981142b7ff34b842b20d4
alberth04 <godoyalberth.2002@gmail.com>
2022-08-26 13:39:23 -0600

    Empezando Laboratorio

---
+++ src/lab6p2_/alberthgodoy/Lab6P2_AlberthGodoy.java
@@ -0,0 +1,17 @@
+
+package lab6p2_.alberthgodoy;
+
+/**
+ *
+ * @author godoy
+ */
+public class Lab6P2_AlberthGodoy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
